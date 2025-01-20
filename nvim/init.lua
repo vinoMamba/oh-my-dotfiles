dcc3d2f4e9f94429e39bcfa6f9a07664995806ec
vino <vino0908@outlook.com>
2025-01-20 21:46:46 +0800

    yazi.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,14 +2,4 @@ require("config.options")
 require("config.keymaps")
 require("config.lazy")
 
-vim.api.nvim_create_autocmd("BufWritePost",{
-	pattern = "lua/config/*.lua",
-  callback = function()
-  	local function reload_module(module_name)
-    	package.loaded[module_name] = nil
-      require(module_name)
-    end
-    reload_module("options")
-    reload_module("keymaps")
-  end
-})
+
