4d14a2b457214f3b51d60c238ab3311a4dc4f39b
vino <vino0908@outlook.com>
2025-01-22 16:12:12 +0800

    add Bonly custom command

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,12 @@
 require("config.options")
 require("config.keymaps")
 require("config.lazy")
+
+vim.api.nvim_create_user_command('Bonly', function()
+  local cb = vim.api.nvim_get_current_buf()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(buf) and buf ~= cb then
+      vim.api.nvim_buf_delete(buf, { force = true })
+    end
+  end
+end, {})
