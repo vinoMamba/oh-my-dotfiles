4f0df5b4e8f4dd3343448c099413bea4b6625ba9
vino <vino0908@outlook.com>
2025-01-24 13:47:31 +0800

    gitsigns

--- nvim/lua/plugins/basic.lua
+++ nvim/lua/plugins/basic.lua
@@ -138,6 +138,23 @@ return {
       winbar = {},
       inactive_winbar = {},
       extensions = {}
+    },
+    -- gitsigns
+    {
+      "lewis6991/gitsigns.nvim",
+      opts = {
+        signcolumn = false,
+        current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+          delay = 100,
+          ignore_whitespace = false,
+          virt_text_priority = 100,
+          use_focus = true,
+        },
+        current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+      },
     }
   }
 }
