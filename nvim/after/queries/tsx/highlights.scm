a018159cb172243ef0b384aeae6c177cec92abde
vino <vino0908@outlook.com>
2025-01-24 22:35:25 +0800

    jsx highlight of classname

--- nvim/after/queries/tsx/highlights.scm
+++ nvim/after/queries/tsx/highlights.scm
@@ -2,3 +2,8 @@
 
 (jsx_opening_element (identifier) @open_tag)
 (jsx_closing_element (identifier) @close_tag)
+(jsx_attribute
+  (property_identifier) @attr_name
+  (string (string_fragment) @attr_classname)
+  (#eq? @attr_name "className")
+)
--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -4,6 +4,24 @@ return {
   config = function()
     require 'nvim-treesitter.configs'.setup {
       auto_install = false,
+      ensure_installed = {
+        "lua",
+        "c",
+        "go",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "scheme",
+        "sql",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
       highlight = {
         enable = true,
       }
