34e99aea7edbd2a27734de5cb85bae33979135c0
vino <vino0908@outlook.com>
2025-07-23 23:03:48 +0800

    mini ai

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -3,6 +3,7 @@ return {
   version = false,
   config = function ()
     require('mini.pairs').setup()
-    require('mini.ai').setup()
+    require('mini.ai').setup({})
   end
 }
+
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,15 @@
 return  {
-  "nvim-treesitter/nvim-treesitter", 
-  branch = 'master', 
-  lazy = false, 
+  "nvim-treesitter/nvim-treesitter",
+  branch = 'master',
+  lazy = false,
   build = ":TSUpdate",
   config = function ()
     require'nvim-treesitter.configs'.setup {
-      auto_install = true,
-      highlight = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true
-      },
-  }
+      ensure_installed = { "lua", "vim", "vimdoc" },
+      highlight = { enable = true },
+      incremental_selection = { enable = true },
+      textobjects = { enable = true },
+    }
   end
 }
+
