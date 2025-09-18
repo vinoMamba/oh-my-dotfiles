20cdcd59d1fd4c6af6506bd9bf85124f3bcc99b9
vino <vino0908@outlook.com>
2025-09-18 11:10:46 +0800

    更新 Ghostty 窗口配置和 Markdown 图片插件

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -109,46 +109,23 @@ return {
     'bullets-vim/bullets.vim',
     ft = { 'markdown' },
   },
-  -- {
-  --   'HakonHarnes/img-clip.nvim',
-  --   ft = { 'tex', 'markdown', 'typst' },
-  --   opts = {
-  --     default = {
-  --       dir_path = './attachments',
-  --       use_absolute_path = false,
-  --       copy_images = true,
-  --       prompt_for_file_name = false,
-  --       file_name = '%y%m%d-%H%M%S',
-  --       extension = 'avif',
-  --       process_cmd = 'magick convert - -quality 75 avif:-',
-  --     },
-  --     filetypes = {
-  --       markdown = {
-  --         template = '![image$CURSOR]($FILE_PATH)',
-  --       },
-  --       tex = {
-  --         dir_path = './figs',
-  --         extension = 'png',
-  --         process_cmd = '',
-  --         template = [[
-  --   \begin{figure}[h]
-  --     \centering
-  --     \includegraphics[width=0.8\textwidth]{$FILE_PATH}
-  --   \end{figure}
-  --       ]], ---@type string | fun(context: table): string
-  --       },
-  --       typst = {
-  --         dir_path = './figs',
-  --         extension = 'png',
-  --         process_cmd = 'magick convert - -density 300 png:-',
-  --         template = [[
-  --         #align(center)[#image("$FILE_PATH", height: 80%)]
-  --         ]],
-  --       },
-  --     },
-  --   },
-  --   keys = {
-  --     { '<leader>P', '<cmd>PasteImage<cr>', desc = 'Paste image from system clipboard' },
-  --   },
-  -- },
+  {
+    'HakonHarnes/img-clip.nvim',
+    ft = { 'tex', 'markdown', 'typst' },
+    opts = {
+      default = {
+         dir_path = "public"
+      },
+      filetypes = {
+        markdown = {
+          template = "![$CURSOR](/$FILE_NAME)",
+          use_absolute_path = false,
+          relative_to_current_file = false
+        }
+      }
+    },
+    keys = {
+      { '<leader>p', '<cmd>PasteImage<cr>', desc = 'Paste image from system clipboard' },
+    },
+  },
 }
