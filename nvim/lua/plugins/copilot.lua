9ed2e3e1d2f8760f108a0f2185606a358fadc5b2
vino <vino0908@outlook.com>
2025-12-19 10:52:23 +0800

    chore(nvim): update plugin versions

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,13 +1,15 @@
-return {
-  "github/copilot.vim",
-  config = function()
-    vim.g.copilot_enabled = true
-    vim.g.copilot_no_tab_map = true
-    vim.cmd('imap <silent><script><expr> <tab> copilot#Accept("")')
-    vim.cmd([[
-  	let g:copilot_filetypes = {
-  	     \ 'TelescopePrompt': v:false,
-  	   \ }
-  	]])
-  end
-}
+return {}
+
+-- return {
+--   "github/copilot.vim",
+--   config = function()
+--     vim.g.copilot_enabled = true
+--     vim.g.copilot_no_tab_map = true
+--     vim.cmd('imap <silent><script><expr> <tab> copilot#Accept("")')
+--     vim.cmd([[
+--   	let g:copilot_filetypes = {
+--   	     \ 'TelescopePrompt': v:false,
+--   	   \ }
+--   	]])
+--   end
+-- }
